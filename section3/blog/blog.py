ab0418f6ccab6ffa1671fa637cdd10c2b308e3f6
Thaissa Falbo <thaissa.falbo@gmail.com>
2018-01-29 22:44:20 -0200

    Add blog tests

---
+++ section3/blog/blog.py
@@ -0,0 +1,17 @@
+class Blog:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.posts = []
+
+    def __repr__(self):
+        return '{} by {}({} post{})'.format(self.title,
+                                           self.author,
+                                           len(self.posts),
+                                           's' if len(self.posts) != 1 else '')
+
+    def create_post(self, title, content):
+        pass
+
+    def json(self):
+        pass
